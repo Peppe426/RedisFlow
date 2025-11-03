3bdac3da341a592076431efb8399ad87b1c64795
Peter Liljeholm <peter.liljeholm@cardiolex.se>
2025-11-03 23:11:37 +0100

    feat(fomain) added

---
+++ src/RedisFlow/RedisFlow.Domain/ValueObjects/Message.cs
@@ -0,0 +1,23 @@
+﻿namespace RedisFlow.Domain.ValueObjects;
+
+public class Message
+{
+    public string Producer
+    {
+        get;
+        private set;
+    }
+
+    public string Content
+    {
+        get;
+    }
+
+    public DateTime CreatedAt { get; } = DateTime.UtcNow;
+
+    public Message(string producer, string content)
+    {
+        Producer = producer;
+        Content = content;
+    }
+}
