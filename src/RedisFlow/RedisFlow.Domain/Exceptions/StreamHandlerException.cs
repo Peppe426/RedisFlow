4cba7ea056d19e0b95f9c00d7dd191700753fcf6
Peter Liljeholm <peter.liljeholm@cardiolex.se>
2025-11-25 23:16:51 +0100

    feat(exceptions): add StreamHandlerException class

---
+++ src/RedisFlow/RedisFlow.Domain/Exceptions/StreamHandlerException.cs
@@ -0,0 +1,16 @@
+﻿namespace RedisFlow.Domain.Exceptions;
+
+internal class StreamHandlerException : Exception
+{
+    public StreamHandlerException()
+    {
+    }
+
+    public StreamHandlerException(string? message) : base(message)
+    {
+    }
+
+    public StreamHandlerException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
