28cc0bdb1d7444f8356c023e8ca9e0e1c2164cde
Peter Liljeholm <peter.liljeholm@cardiolex.se>
2025-11-25 19:15:51 +0100

    feat(domain): add AggregateRoot class with event handling

---
+++ src/RedisFlow/RedisFlow.Domain/Base/AggregateRoot.cs.cs
@@ -0,0 +1,20 @@
+﻿namespace RedisFlow.Domain.Base;
+
+using System;
+
+public record AggregateRoot
+{
+    /// <summary>
+    /// Subscribe to this event to react to domain events as they are raised.
+    /// </summary>
+    public static event Action<IDomainEvent>? DomainEventRaised;
+
+    /// <summary>
+    /// Invokes the DomainEventRaised event for subscribers.
+    /// </summary>
+    /// <param name="domainEvent">The domain event to raise.</param>
+    public void RaiseDomainEvent(IDomainEvent domainEvent)
+    {
+        DomainEventRaised?.Invoke(domainEvent);
+    }
+}
