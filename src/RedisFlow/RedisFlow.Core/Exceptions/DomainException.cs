97404797d2a117feefa265537674a8c4b395b6d8
Peter Liljeholm <peter.liljeholm@cardiolex.se>
2025-11-26 22:29:40 +0100

    feat(exceptions): add DomainException base class

---
+++ src/RedisFlow/RedisFlow.Core/Exceptions/DomainException.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace RedisFlow.Core.Exceptions;
+
+/// <summary>
+/// Base exception for domain errors. Provides guard methods for argument validation.
+/// </summary>
+public class DomainException : Exception
+{
+    public DomainException()
+    {
+    }
+
+    public DomainException(string? message) : base(message)
+    {
+    }
+
+    public DomainException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    /// <summary>
+    /// Throws an exception if argument is null, empty, or consists only of white-space characters.
+    /// </summary>
+    /// <param name="argument">The string argument to validate.</param>
+    /// <param name="paramName">The name of the parameter with which argument corresponds.</param>
+    /// <exception cref="ArgumentNullException">argument is null.</exception>
+    /// <exception cref="ArgumentException">argument is empty or consists only of white-space characters.</exception>
+    public static void ThrowIfNullOrWhiteSpace(
+        [NotNull] string? argument,
+        [CallerArgumentExpression("argument")] string? paramName = null)
+    {
+        if (argument is null)
+            throw new ArgumentNullException(paramName);
+        if (string.IsNullOrWhiteSpace(argument))
+            throw new ArgumentException("Argument cannot be empty or whitespace.", paramName);
+    }
+}
